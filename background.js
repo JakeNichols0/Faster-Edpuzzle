a1cba7eb00faf23678551b56d1fc7d340418dfbf
JakeNichols0 <adages.shows_03@icloud.com>
2025-04-09 13:47:09 -0500

    I fixed it Yay!!!

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.runtime.onInstalled.addlistener(() => {
-    chrome.storage.sync.set({speed: 1}).then(() => {
+    chrome.storage.sync.set({ speed: 1 }).then(() => {
         console.log("Speed has been initalized to 1.");
     });
 });
